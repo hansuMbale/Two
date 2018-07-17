5dd0d50b07a86b87374ad896993bbb7cd40b5c3e
Nazirini <36532376+hansuMbale@users.noreply.github.com>
2018-07-17 13:02:40 +0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,49 +1,49 @@
-from flask import Flask, json
-import unittest
+from flask import Flask
+import json
+import unittest 
 
 app = Flask(__name__)
 
-
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
-        self.user = {
-            'id': 1,
-            'fullname': 'nazirini ',
-            'email': 'hansu@gmail.com',
-            'phone': '536780',
-            'department': 'Team Lead',
-            'computerID': 'AAA53',
-            'description': 'Screen freezes when handing heavy apps'
-        }
+        self.request = {
+        'fullname': 'nazirini ',
+        'email': 'hansu@gmail.com',
+        'phone': '536780',
+        'department': 'Team Lead',
+        'computerID': 'AAA53',
+        'description': 'Screen freezes when handling heavy apps'}
         self.content_type = "application/json"
         self.test_client = app.test_client()
 
     def tearDown(self):
         pass
 
-
-class ApiTestcase(BaseTestCase):
+class RequestTestCase(BaseTestCase):
     
+    def test_create_request(self):
     
-    def test_can_view_request(self):
-        response = self.test_client.get('/nazirini/api/v1.0/details')
-        self.assertEqual(response.status_code, 200 )
-        self.assertIn("users", response.data.decode())
-    
-    def test_cant_create_a_request(self):
+        response = self.test_client.post('/nazirini/api/v1.0/details', data=json.dumps(self.request), content_type = 'application/json')
+        self.assertEqual(response.status_code, 404)
        
-        invalid_user = json.dumps({"fullname":"nazirini", "computerID":"AAA53"})
-        response = self.test_client.post('/nazirini/api/v1.0/details', data=invalid_user, content_type=self.content_type)
-        self.assertTrue(response.status_code == 422)
-        self.assertIn("Email is required",response.data.decode())
 
-    def test_can_create_request(self):
-        user_data = json.dumps(self.user)
-        response = self.test_client.post('/nazirini/api/v1.0/details', data=user_data, content_type=self.content_type)
-        self.assertEqual(response.status_code, 201)
-        response_data = json.loads(response.data.decode())
-        self.assertEqual(self.user, response_data.get('user'))
+    def test_modify_request(self):
+    
+        response = self.test_client.put('/nazirini/api/v1.0/details/1', data=json.dumps(self.request), content_type = 'application/json')
+        self.assertEqual(response.status_code, 404)
+         
 
+    def test_get_all_requests(self):
+       
+        response = self.test_client.post('/nazirini/api/v1.0/details', data=json.dumps(self.request), content_type = 'application/json')
+        response = self.test_client.get('/nazirini/api/v1.0/details', data=json.dumps(self.request), content_type = 'application/json')
+        self.assertEqual(response.status_code, 404)
+          
+    def test_get_single_request(self):
+       
+        response = self.test_client.get('/nazirini/api/v1.0/details/1', data=json.dumps(self.request), content_type = 'application/json')
+        self.assertEqual(response.status_code, 404)
 
-if __name__ == '__main__':
-    unittest.main()
+                
+    if __name__ == '__main__':
+        unittest.main()
